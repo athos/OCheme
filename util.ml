cf1dfbfdc7da4872fd1addd21799c77081b12806
OHTA Shogo <athos0220@gmail.com>
2011-03-19 13:03:18 +0900

    added utils

---
+++ util.ml
@@ -0,0 +1,4 @@
+let (@@) f x = f x
+let id x = x
+let (|>) x f = f x
+let const c _ = c  
---
+++ util.mli
@@ -0,0 +1,4 @@
+val ( @@ ) : ('a -> 'b) -> 'a -> 'b
+val id : 'a -> 'a
+val ( |> ) : 'a -> ('a -> 'b) -> 'b
+val const : 'a -> 'b -> 'a
