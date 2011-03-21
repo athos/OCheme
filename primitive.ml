f7ea814daf22cfd068cb9a757d024a99d781e34d
OHTA Shogo <athos0220@gmail.com>
2011-03-21 18:31:51 +0900

    added more primitive functions

--- primitive.ml
+++ primitive.ml
@@ -1,5 +1,6 @@
 open Util
-open Value
+
+module V = Value
 
 let assert_pred pred =
   if pred () then
@@ -8,21 +9,21 @@ let assert_pred pred =
     raise Vm.Runtime_error
 
 let arithmetic op e xs =
-  assert_pred (fun () -> List.for_all is_number xs);
-  from_int @@ List.fold_left (fun m n -> op m @@ to_int n) e xs
+  assert_pred (fun () -> List.for_all V.is_number xs);
+  V.from_int @@ List.fold_left (fun m n -> op m @@ V.to_int n) e xs
 
 let add = arithmetic (+) 0
 let mul = arithmetic ( * ) 1
 let sub = function
     [] -> raise Vm.Runtime_error
-  | [x] -> from_int (- (to_int x))
+  | [x] -> V.from_int (- (V.to_int x))
   | x::xs ->
-      arithmetic (-) (to_int x) xs
+      arithmetic (-) (V.to_int x) xs
 let div = function
     [] -> raise Vm.Runtime_error
-  | [x] -> from_int 0
+  | [x] -> V.from_int 0
   | x::xs ->
-      arithmetic (/) (to_int x) xs
+      arithmetic (/) (V.to_int x) xs
 
 let equal xs =
   let rec equal2 x y =
@@ -34,11 +35,32 @@ let equal xs =
       | (Vm.Pair (x1,x2)), (Vm.Pair (y1,y2)) -> x = y && equal2 x2 y2
       | _,                    _              -> false
   in match xs with
-      [x; y] -> from_bool @@ equal2 x y
+      [x; y] -> V.from_bool @@ equal2 x y
     | _ -> raise Vm.Runtime_error
 
+let cons = function
+    [x; y] -> V.cons x y
+  | _ -> raise Vm.Runtime_error
+
+let car = function
+    [x] -> V.car x
+  | _ -> raise Vm.Runtime_error
+
+let cdr = function
+    [x] -> V.cdr x
+  | _ -> raise Vm.Runtime_error
+
+let display = function
+    [x] -> print_string @@ V.show x; V.nil
+  | _ -> raise Vm.Runtime_error
+
+let newline = function
+    [] -> print_newline (); V.nil
+  | _ -> raise Vm.Runtime_error
+
 let primitives =
-  [("+", add); ("-", sub); ("*", mul); ("/", div); ("equal?", equal)]
+  [("+", add); ("-", sub); ("*", mul); ("/", div); ("equal?", equal);
+   ("cons", cons); ("car", car); ("cdr", cdr); ("display", display); ("newline", newline)]
 
 let load_primitives env =
   List.fold_left
