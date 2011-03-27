2e2baee44bc3a7c122345a82efaf2c1e56c63284
OHTA Shogo <athos0220@gmail.com>
2011-03-27 18:00:16 +0900

    modified main module for reflecting changes

--- main.ml
+++ main.ml
@@ -3,5 +3,5 @@ open Util
 let _ =
   let s = Stream.of_channel stdin in
   let c = Syntax.from_value @@ Reader.read s in
-  let b = Compiler.compile c Vm.Halt in
-    Vm.run @@ Vm.initial_state b Vm.empty_env
+  let b = Compiler.compile c (Primitive.define_primitives ()) Vm.Halt in
+    Vm.run @@ Vm.initial_state b @@ Primitive.load_primitives ()
