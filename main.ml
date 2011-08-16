0eaa17a12a3151adfd3ab336d521b78fbc0c1234
OHTA Shogo <athos0220@gmail.com>
2011-08-16 15:35:11 +0900

    rewrote main routine using eval

--- main.ml
+++ main.ml
@@ -2,6 +2,4 @@ open Util
 
 let _ =
   let s = Stream.of_channel stdin in
-  let c = Syntax.from_value @@ Reader.read s in
-  let b = Compiler.compile c Compiler.empty_cenv Vm.Halt in
-  Vm.run @@ Vm.initial_state b @@ Primitive.load_primitives ()
+  Primitive.eval (Reader.read s) (Primitive.standard_env ())
