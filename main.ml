3b2fc900420f6410a5e10c1f275d40f158ccae22
OHTA Shogo <athos0220@gmail.com>
2011-08-16 16:05:04 +0900

    added simple REPL

--- main.ml
+++ main.ml
@@ -2,4 +2,17 @@ open Util
 
 let _ =
   let s = Stream.of_channel stdin in
-  Primitive.eval (Reader.read s) (Primitive.standard_env ())
+  let env = Primitive.standard_env () in
+  let rec loop () =
+    begin
+      try
+        print_string "ocheme> ";
+        flush stdout;
+        let exp = Reader.read s in
+        Printf.printf "%s\n" @@ Value.show @@ Primitive.eval exp env
+      with
+        Vm.Runtime_error msg ->
+          Printf.printf "error: %s\n" msg
+    end;
+    loop()
+  in loop ()
